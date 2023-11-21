9f239e22a5905c5c4b3ec4394508eaab487ea5b3
William Bout <hello@williambout.me>
2023-11-21 13:25:16 +0100

    Viewport selection

--- internal/base.js
+++ internal/base.js
@@ -2,22 +2,29 @@ import { Pane } from "tweakpane";
 import * as EssentialsPlugin from "@tweakpane/plugin-essentials";
 
 const options = {
-  viewport: "mobile",
+  viewport: "desktop",
 };
+
+const viewport = document.querySelector("._viewport");
+viewport.className = `_viewport ${options.viewport}`;
 
 const pane = new Pane();
 pane.registerPlugin(EssentialsPlugin);
-pane.addBinding(options, "viewport", {
-  options: {
-    mobile: "mobile",
-  },
-});
+pane
+  .addBinding(options, "viewport", {
+    options: {
+      mobile: "mobile",
+      desktop: "desktop",
+    },
+  })
+  .on("change", (event) => {
+    viewport.className = `_viewport ${event.value}`;
+  });
 
 const fpsGraph = pane.addBlade({
   view: "fpsgraph",
-
   label: "fps",
-  rows: 1,
+  rows: 2,
 });
 
 function render() {
